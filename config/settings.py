3894b3025534506a2f736f238bba29a33a004b2f
Оля <@DESKTOP-HU1OG43>
2026-05-12 13:00:14 +0300

    фикс статики

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
